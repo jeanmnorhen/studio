ec03c20a57e95e764ef260677871c37142024577
Jean Leite <jeanmdasleite@gmail.com>
2025-05-27 23:45:05 +0000

    vercel retornou Command "npm run build" exited with 1

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true, // Temporariamente removido para depuração do build
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
